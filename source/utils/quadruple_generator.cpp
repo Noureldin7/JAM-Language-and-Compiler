bbd906d36f5eaeff327405ed4ad1852aa5681f53
John Maurice <johnmaurice71@gmail.com>
2023-05-19 15:22:26 +0300

    added quadruple generator class

--- source/utils/quadruple_generator.cpp
+++ source/utils/quadruple_generator.cpp
@@ -1,29 +1,27 @@
-#include "symbol_table.hpp"
-#include "enums.hpp"
-#include <fstream>
-// create a global fstream writer
+#include "quadruple_generator.hpp"
 
-symbol* not_op(symbol* op){
+
+symbol* quadruple_generator::not_op(symbol* op){
     // print eni b3ks el operation eli 3ndy
     // write it in quadruples file
     // create new boolean symbol temp 
     // return pointer to that new symbol 
 }
 // add , minus , mul , divide
-symbol* arth_op(ops operation , symbol* op1 , symbol* op2){
+symbol* quadruple_generator::arth_op(ops operation , symbol* op1 , symbol* op2){
     // print eni b3ks el operation eli 3ndy
     // write it in quadruples file
     // create new symbol temp 
     // return pointer to that new symbol 
 }
 
-symbol* plus_op(symbol* op1 , symbol* op2){
+symbol* quadruple_generator::plus_op(symbol* op1 , symbol* op2){
     // check that op1 and op2 are not strings 
     // if they are not strings call arth_op
     // else call concat_op
 }
 
-symbol* concat_op(symbol* op1 , symbol* op2){
+symbol* quadruple_generator::concat_op(symbol* op1 , symbol* op2){
     
 }
 
@@ -42,6 +40,3 @@ symbol* concat_op(symbol* op1 , symbol* op2){
 // 12- concat_op(+) --> ATAREK
 
 // for each op , validate the type match --> return error if not matched 
-
-
-
---
+++ source/utils/quadruple_generator.hpp
@@ -0,0 +1,26 @@
+#include <fstream>
+#include "utils.hpp"
+#include "symbol_table.hpp"
+#include "enums.hpp"
+
+
+class quadruple_generator {
+    fstream writer;
+
+public:
+    quadruple_generator(string filename);
+    // these print conversion quadruples to file
+    // assuming the input op is convertable to this type
+    // otherwise it throws a yyerror
+    symbol* Int(symbol* op);
+    symbol* Double(symbol* op);
+    symbol* String(symbol* op);
+    symbol* Bool(symbol* op);
+
+    symbol* not_op(symbol* op);
+    symbol* arth_op(ops operation , symbol* op1 , symbol* op2);
+    symbol* plus_op(symbol* op1 , symbol* op2);
+    symbol* concat_op(symbol* op1 , symbol* op2);
+
+    ~quadruple_generator();
+};
